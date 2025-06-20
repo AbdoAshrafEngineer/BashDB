54d0d9c23654824141a3ccfa1004a9a6364047b3
Abdelrahman <abdelrahman.ashraf.engineer@gmail.com>
2025-06-20 09:33:36 +0300

    add exit in selection

--- table_ops.sh
+++ table_ops.sh
@@ -426,7 +426,7 @@ update_row()
 #=========================================================================
 
 PS3="Choose table operation (press Enter to show menu again): "
-select op in list_all_tables create insert select drop delete update 
+select op in list_all_tables create insert select drop delete update exit
 do 
     case $op in 
         list_all_tables)
